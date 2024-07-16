7f55c991c2e2be960e06dbb3b00b0291bf130136
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-07-16 20:03:55 +0300

    Add project files.

---
+++ AgricultureApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgricultureApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
